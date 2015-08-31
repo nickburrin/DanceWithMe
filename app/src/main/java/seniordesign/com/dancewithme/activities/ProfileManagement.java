0e582d59423c485abfd265a25cb6e7f0393915a9
Nick Burrin <NickBurrin@utexas.edu>
2015-08-31 14:49:54 -0500

    Started working on layout for Profile Management

--- app/src/main/java/seniordesign/com/dancewithme/activities/ProfileManagement.java
+++ app/src/main/java/seniordesign/com/dancewithme/activities/ProfileManagement.java
@@ -4,17 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 import seniordesign.com.dancewithme.R;
 
 public class ProfileManagement extends ActionBarActivity {
 
+    private EditText mUsernameView;
+    private ImageButton mProfPic;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_management);
+        ((TextView) findViewById(R.id.username)).setText(ParseUser.getCurrentUser().get("first_name")
+                + " " + ParseUser.getCurrentUser().get("last_name"));
+
+        mProfPic = (ImageButton) findViewById(R.id.profPic);
+        mProfPic.setImageDrawable(null); //fill with an image
     }
 
+    public void changeProfPic(View v) {
+        //do stuff
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

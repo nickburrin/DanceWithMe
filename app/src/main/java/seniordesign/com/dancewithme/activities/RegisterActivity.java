7e30b2c8079205bf7a125820573bbe0cced88199
Nick Burrin <NickBurrin@utexas.edu>
2015-10-01 11:01:00 -0500

    Still working on RegisterActivity, almost done

--- app/src/main/java/seniordesign/com/dancewithme/activities/RegisterActivity.java
+++ app/src/main/java/seniordesign/com/dancewithme/activities/RegisterActivity.java
@@ -3,22 +3,39 @@ package seniordesign.com.dancewithme.activities;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
+import com.parse.FindCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
+
+import java.text.ParseException;
+import java.util.List;
 
 import seniordesign.com.dancewithme.R;
+import seniordesign.com.dancewithme.adapters.CustomOnItemSelectedListener;
 
 
 public class RegisterActivity extends ActionBarActivity {
+
+    // UI fields
+    private EditText mFirstName;
+    private EditText mLastName;
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private EditText mConfirmPasswordView;
+    private Spinner mGenderSpinner;
+    private Button mRegisterButton;
+    private Button mExistingUserButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,27 +48,30 @@ public class RegisterActivity extends ActionBarActivity {
             // TODO: fill in username box
         }
 
-        Spinner spinner = (Spinner) findViewById(R.id.gender_spinner);
-        spinner.setOnItemSelectedListener(this);
+        mEmailView = ((EditText) findViewById(R.id.et_email));
+        mPasswordView = ((EditText) findViewById(R.id.et_password));
+        mConfirmPasswordView = ((EditText) findViewById(R.id.et_confirm_password));
+        mFirstName = ((EditText) findViewById(R.id.et_first_name));
+        mLastName = ((EditText) findViewById(R.id.et_last_name));
 
-        /*
-                ParseUser user = new ParseUser();
-                user.setUsername(username);
-                user.setPassword(password);
+        mGenderSpinner = ((Spinner) findViewById(R.id.spinner_gender));
+        mGenderSpinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
 
-                user.signUpInBackground(new SignUpCallback() {
-                    public void done(com.parse.ParseException e) {
-                        if (e == null) {
-                            Intent i = new Intent(LoginActivity.this, SelectAVenue.class);
-                            startActivity(i);
-                        } else {
-                            Toast.makeText(getApplicationContext(),
-                                    "There was an error signing up."
-                                    , Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
-        */
+        mExistingUserButton = (Button) findViewById(R.id.button_existing_user);
+        mExistingUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        mRegisterButton = (Button) findViewById(R.id.button_submit_new_account);
+        mRegisterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccount();
+            }
+        });
     }
 
     @Override
@@ -75,6 +95,132 @@ public class RegisterActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    public void createAccount(){
+        // Only create an account if all fields check out
+        if(verifyUIFields() == true) {
+            // extract all UI fields, create ParseUser
+            ParseUser user = extractParseUser();
+
+            // push to Parse
+
+/*
+            ParseUser user = new ParseUser();
+            user.setUsername(username);
+            user.setPassword(password);
+
+            user.signUpInBackground(new SignUpCallback() {
+                public void done(com.parse.ParseException e) {
+                    if (e == null) {
+                        Intent i = new Intent(LoginActivity.this, SelectAVenue.class);
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "There was an error signing up."
+                                , Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+*/
+        }
+    }
+
+    private boolean verifyUIFields() {
+        boolean fieldsOK = false;
+        View focusView = null;
+
+        if(mFirstName.getText().toString().isEmpty()){
+            // Check for first name
+            mFirstName.setError(getString(R.string.error_field_required));
+            focusView = mFirstName;
+            fieldsOK = true;
+        }
+        if(mLastName.getText().toString().isEmpty()){
+            // Check for last name
+            mLastName.setError(getString(R.string.error_field_required));
+            focusView = mLastName;
+            fieldsOK = true;
+        }
+        if (TextUtils.isEmpty(mPasswordView.getText().toString())) {
+            // Check if the user entered password
+            mPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mPasswordView;
+            fieldsOK = true;
+        }
+        if (TextUtils.isEmpty(mConfirmPasswordView.getText().toString())) {
+            // Check if the user entered confirm password
+            mConfirmPasswordView.setError(getString(R.string.error_field_required));
+            focusView = mConfirmPasswordView;
+            fieldsOK = true;
+        }
+        if (TextUtils.isEmpty(mEmailView.getText().toString())) {
+            // Check if user entered email
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            fieldsOK = true;
+        }
+        if (!isEmailValid(mEmailView.getText().toString())) {
+            // Check for a valid email address.
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            fieldsOK = true;
+        }
+        if(!doPasswordsMatch(mPasswordView.getText().toString(), mConfirmPasswordView.getText().toString())){
+            // Check if confirm password matches the original, if the user entered one.
+            mConfirmPasswordView.setError(getString(R.string.error_password_no_match));
+            focusView = mConfirmPasswordView;
+            fieldsOK = true;
+        }
+
+        if (fieldsOK) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        }
+
+        return fieldsOK;
+    }
+
+    private boolean isEmailValid(String email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean doPasswordsMatch(String pass, String confirm) {
+        if(pass.isEmpty() || confirm.isEmpty()) {
+            return false;
+        } else if(pass.equals(confirm)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    private ParseUser extractParseUser() {
+        String email = ((EditText) findViewById(R.id.et_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
+        String confirmPassword = ((EditText) findViewById(R.id.et_confirm_password)).getText().toString();
+        String firstName = ((EditText) findViewById(R.id.et_first_name)).getText().toString();
+        String lastName = ((EditText) findViewById(R.id.et_last_name)).getText().toString();
+        String gender = ((Spinner) findViewById(R.id.spinner_gender)).getSelectedItem().toString();
+
+        ParseQuery<ParseUser> query = ParseUser.getQuery();
+        query.whereEqualTo("gender", "female");
+        query.findInBackground(new FindCallback<ParseUser>() {
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null) {
+                    // The query was successful.
+                } else {
+                    // Something went wrong.
+                }
+            }
+        });
+
+        if(query.)
+        ParseUser newUser = new ParseUser();
+
+    }
+
 
     public void displaySelectAVenue(View view)
     {

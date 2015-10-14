04d962328454413e95ea67b42432ca49bba247bc
Nick Burrin <NickBurrin@utexas.edu>
2015-10-14 15:00:52 -0500

    Added pojos for Matches and Event

--- app/src/main/java/seniordesign/com/dancewithme/activities/MyApplication.java
+++ app/src/main/java/seniordesign/com/dancewithme/activities/MyApplication.java
@@ -9,6 +9,8 @@ import com.parse.ParseObject;
 
 import seniordesign.com.dancewithme.pojos.DanceStyle;
 import seniordesign.com.dancewithme.pojos.Dancehall;
+import seniordesign.com.dancewithme.pojos.Event;
+import seniordesign.com.dancewithme.pojos.Matches;
 
 public class MyApplication extends Application {
     @Override
@@ -17,6 +19,8 @@ public class MyApplication extends Application {
         Parse.enableLocalDatastore(this);
         ParseObject.registerSubclass(DanceStyle.class);
         ParseObject.registerSubclass(Dancehall.class);
+        ParseObject.registerSubclass(Matches.class);
+        ParseObject.registerSubclass(Event.class);
         Parse.initialize(this, "Q7azOG47hd1mk0cPmwhUTm3DKcKapSHQepzNdSrd", "NA1kTUjHQqc2vbLDp5ywGvCBMGtLp6EKsAc91nxT");
         ParseInstallation.getCurrentInstallation().saveInBackground();
         // Specify an Activity to handle all pushes by default.
---
+++ app/src/main/java/seniordesign/com/dancewithme/pojos/Event.java
@@ -0,0 +1,45 @@
+package seniordesign.com.dancewithme.pojos;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by nickburrin on 10/14/15.
+ */
+@ParseClassName("Event")
+public class Event extends ParseObject {
+    public Event() {}
+
+    public Date getDate(){
+        return getDate("date");
+    }
+
+    public void setDate(Date d){
+        put("date", d);
+    }
+
+    public String getTitle(){
+        return getString("title");
+    }
+
+    public void setTitle(String t){
+        put("title", t);
+    }
+
+    public ArrayList<ParseUser> getAttendees(){
+        return (ArrayList<ParseUser>) get("attendees");
+    }
+
+    public void setAttendees(ArrayList<ParseUser> list){
+        put("attendees", list);
+    }
+
+    public void addAttendee(ParseUser p){
+        getAttendees().add(p);
+        this.saveInBackground();
+    }
+}

b7d35a9e37e043268eab0057823d7efdb1d6c5d8
Nick Burrin <NickBurrin@utexas.edu>
2015-10-15 10:24:29 -0500

    Cleaned up MatchFragment, need to implement changes proposed in comments of conversationList()

--- app/src/main/java/seniordesign/com/dancewithme/fragments/HomeTabFragment.java
+++ app/src/main/java/seniordesign/com/dancewithme/fragments/HomeTabFragment.java
@@ -1,28 +1,20 @@
 package seniordesign.com.dancewithme.fragments;
 
-import android.app.Activity;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import seniordesign.com.dancewithme.R;
 import seniordesign.com.dancewithme.activities.HomeActivity;
 import seniordesign.com.dancewithme.activities.MyApplication;
 
 public class HomeTabFragment extends Fragment {
     HomeActivity activity;
     MyApplication application;
-    //User user;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         activity = (HomeActivity) getActivity();
         application = (MyApplication) activity.getApplication();
-        //user = DataStore.getInstance().getUser();
     }
 
 }

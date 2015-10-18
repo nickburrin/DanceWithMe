e71de9e1af95b585212b6725ee45b360d21d2eb5
Nick Burrin <NickBurrin@utexas.edu>
2015-10-17 19:46:43 -0500

    fixed merge conflicts

--- app/src/main/java/seniordesign/com/dancewithme/fragments/MessageFragment.java
+++ app/src/main/java/seniordesign/com/dancewithme/fragments/MessageFragment.java
@@ -39,15 +39,12 @@ public class MessageFragment extends HomeTabFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_message, container, false);
         showSpinner();
-
         return view;
     }
 
